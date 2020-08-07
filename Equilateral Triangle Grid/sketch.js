6a4768aa24d7f5a75271e8a4fe0efc81bafe021b
Caleb Harmon <calebmharmon@gmail.com>
2020-08-07 13:51:31 -0700

    can select a triangle

--- Equilateral Triangle Grid/sketch.js
+++ Equilateral Triangle Grid/sketch.js
@@ -1,67 +1,76 @@
-let R = 10;
+let R = 30;
 let N = 10;
+let x1 = 0;
+let y1 = 0;
 
 function setup() {
-  createCanvas(N * 2 * R * cos(PI / 6), N * (R + R * sin(PI / 6)));  
+  createCanvas(N * 2 * R * cos(PI / 6), N * (R + R * sin(PI / 6)));
   drawTriangleGrid();
   //drawTriangleDown(0,25*sin(PI/6),25);
   //save("test.png");
-
+  frameRate(10);
 }
 
-function draw() {}
-
-function drawTriangleGrid() {
-  for (var i = 0; i < N; i++) {
-    var x = 0;
-    if (i % 2 == 0) {
-      x = -1 * R * cos(PI - PI / 6);
+function draw() {
+  drawTriangleGrid();
+  x1 += 1;
+  if (x1 > N * 2 + 2) {
+    x1 = 0;
+    y1 += 1;
+    if (y1 > N) {
+      y1 = 0;
     }
-    drawTriangleRow(x, R + i * (R + R * sin(PI / 6)), R, N + 2);
   }
 }
 
-function drawTriangleRow(x, y, r, triangleCount) {
+function drawTriangleGrid() {
+  for (var i = 0; i < N; i++) {
+    var xPos = 0;
+    if (i % 2 == 0) {
+      xPos = -1 * R * cos(PI - PI / 6);
+    }
+    drawTriangleRow(xPos, R + i * (R + R * sin(PI / 6)), R, N + 2, i);
+  }
+}
+
+function drawTriangleRow(x, y, r, triangleCount, row) {
+  noStroke();
   var l = r * cos(PI / 6) - r * cos(PI - PI / 6);
   for (var i = 0; i < triangleCount; i++) {
+    if (y1 == row && x1 == i * 2 + 1) {
+      fill(0);
+    } else {
+      fill(255,50);
+    }
     drawTriangle(x + l * i, y, r)
+    if (y1 == row && x1 == i * 2) {
+      fill(0);
+    } else {
+      fill(255,50);
+    }
     drawTriangleDown(x - r * cos(PI / 6) + l * i, y - r + r * sin(PI / 6), r);
   }
 }
 
 function drawTriangle(x, y, r) {
+  //setColors();
+  triangle(x, y - r,
+    x + r * cos(PI / 6), y + r * sin(PI / 6),
+    x + r * cos(PI - PI / 6), y + r * sin(PI - PI / 6));
+}
+
+function setColors() {
+  let c = color(100, 100, 255);
   if (random(1) > .5) {
-    fill(100, 255, 100);
-    stroke(100, 255, 100);
-  } else {
-    fill(100, 100, 255);
-    stroke(100, 100, 255);
+    c = color(100, 255, 100);
   }
-  beginShape();
-  vertex(x, y - r);
-  vertex(x + r * cos(PI / 6), y + r * sin(PI / 6));
-  vertex(x + r * cos(PI - PI / 6), y + r * sin(PI - PI / 6));
-  endShape(CLOSE);
-  // stroke(0);
-  // strokeWeight(2);
-  // point(x, y);
+  fill(c);
+  stroke(c);
 }
 
 function drawTriangleDown(x, y, r) {
-  noStroke();
-  if (random(1) > .5) {
-    fill(100, 255, 100);
-    stroke(100, 255, 100);
-  } else {
-    fill(100, 100, 255);
-    stroke(100, 100, 255);
-  }
-  beginShape();
-  vertex(x, y + r);
-  vertex(x + r * cos(PI / 6), y - r * sin(PI / 6));
-  vertex(x + r * cos(PI - PI / 6), y - r * sin(PI - PI / 6));
-  endShape(CLOSE);
-  // stroke(0);
-  // strokeWeight(2);
-  // point(x, y);
+  //setColors();
+  triangle(x, y + r,
+    x + r * cos(PI / 6), y - r * sin(PI / 6),
+    x + r * cos(PI - PI / 6), y - r * sin(PI - PI / 6));
 }
